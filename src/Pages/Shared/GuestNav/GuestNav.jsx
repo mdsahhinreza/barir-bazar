0b4c30ecca81b56cfbe48fd354033e540a248705
mdsahhinreza <mdsahhinreza@gmail.com>
2023-09-13 17:39:31 +0600

    Navbar Desin

--- src/Pages/Shared/GuestNav/GuestNav.jsx
+++ src/Pages/Shared/GuestNav/GuestNav.jsx
@@ -2,26 +2,47 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+
 import { Link } from 'react-router-dom';
+import NavbarSearch from '../NavbarSearch/NavbarSearch';
+import '../../../Assets/css/navbar.css';
 
 const GuestNav = () => {
     return (
-        <Navbar expand="lg" className="navbar navbar-expand-lg navbar-light"  >
-            <Container >
-                <Navbar.Brand href="/">Barir Bazar</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="ms-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <Link className='nav-link' to="/">Home</Link>
-
+        <>
+            <Navbar expand="lg" className="navbar navbar-expand-lg"  >
+                <Container >
+                    <Navbar.Brand href="/" className='fw-bolder'>Barir Bazar</Navbar.Brand>
+                    <Nav className="justify-content-center" activeKey="/home">
+                        <NavbarSearch></NavbarSearch>
+                        <div className='ps-2 navbar-button-div'>
+                            <button className='navbar-shopping-btn me-2'><i class="fa-solid fa-cart-shopping"></i></button>
+                            <button className='navbar-user-btn'>
+                                <i class="fa-solid fa-user-gear"></i>
+                            </button>
+                        </div>
                     </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar >
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                </Container>
+            </Navbar >
+            <Navbar expand="lg" className="navbar navbar-expand-lg bg-dark navbar-dark"  >
+                <Container>
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="m-auto my-2 my-lg-0 text-center"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Link className='nav-link mx-md-3' to="/"><i class="fa-solid fa-house"></i> Home</Link>
+                            <Link className='nav-link mx-md-3' to="/"><i class="fa-solid fa-burger"></i> Food</Link>
+                            <Link className='nav-link mx-md-3' to="/"><i class="fa-solid fa-bag-shopping"></i> Grocery</Link>
+                            <Link className='nav-link mx-md-3' to="/"><i class="fa-solid fa-stethoscope"></i> Pharmacy</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar >
+        </>
+
     )
 }
 
---
+++ src/Pages/Shared/NavbarSearch/NavbarSearch.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import '../../../Assets/css/navbar.css'
+
+const NavbarSearch = () => {
+    return (
+        <Form className="d-flex align-items-center">
+
+            <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2 "
+                aria-label="Search"
+                style={{ width: "750px" }}
+            />
+            <button className='location-picker'><i class="fa-solid fa-location-crosshairs"></i></button>
+
+            <Button variant="outline-success">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </Button>
+        </Form>
+    )
+}
+
+export default NavbarSearch
